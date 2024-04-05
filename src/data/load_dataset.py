3ee47cbc56888a9046020311dc7a37a6725d8441
ronilpatil <ronilpatil2000@gmail.com>
2024-04-05 22:33:46 +0530

    implement function to load data from s3 bucket

--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -1,30 +1,41 @@
 import pathlib
 import yaml
+import boto3
 import pandas as pd
+from io import StringIO
 from src.logger import infologger
 
 infologger.info('*** Executing: load_dataset.py ***')
 
-# load data from given path and return df
-def extract_data(remote_loc: str) -> pd.DataFrame : 
-     try : 
-          # correct way to read data from drive
-          remote_loc = 'https://drive.google.com/uc?id=' + remote_loc.split('/')[-2]  
-          df = pd.read_csv(remote_loc)  
+# load data from S3 bucket and return df
+def extract_data(bucket: str, key: str, access_key: str, secret_key: str) -> pd.DataFrame : 
+     try :
+          # create client to interact with amazon s3 service
+          s3_client = boto3.client('s3', aws_access_key_id = access_key, 
+                                         aws_secret_access_key = secret_key)
+
+          # retrieve the file WineQT.csv from bucket
+          response = s3_client.get_object(Bucket = bucket, Key = key)
+
+          # read the content of WineQT.csv file from bucket as string
+          object_content = response['Body'].read().decode('utf-8')
+
+          # read the data from object_content
+          df = pd.read_csv(StringIO(object_content))
      except Exception as e : 
-          infologger.info(f'there\'s some issue while loading data from remote server [check load_data()]. exc: {e}')
+          infologger.info(f'there\'s some issue while loading data from S3 bucket [check load_data()]. exc: {e}')
      else : 
-          infologger.info(f'data loaded from {remote_loc}')
+          infologger.info(f'data loaded successfully from {bucket}')
           return df
 
 # save data at data/raw dir
 def save_data(data: pd.DateOffset, output_path: str, file_name: str) -> None : 
      try : 
-          data.to_csv(path_or_buf = output_path + f'/{file_name}.csv', index = False)
+          data.to_csv(path_or_buf = output_path + f'/{file_name}', index = False)
      except Exception as e : 
           infologger.info(f'there\'s some issue while saving the data [check save_data()]. exc: {e}')
      else : 
-          infologger.info(f'data saved at [path: {output_path}/{file_name}.csv]')
+          infologger.info(f'data saved at [path: {output_path}/{file_name}]')
 
 # load data & then save it
 def main() -> None : 
@@ -32,16 +43,19 @@ def main() -> None :
      home_dir = curr_dir.parent.parent.parent
 
      params_file = home_dir.as_posix() + '/params.yaml'
+     secret_file = home_dir.as_posix() + '/secrets.yaml'
      try : 
           params = yaml.safe_load(open(params_file))
+          sc_params = yaml.safe_load(open(secret_file))
      except Exception as e : 
-          infologger.info(f'there\'s some issue while loading the params file or output path [check main()]. exc: {e}')
+          infologger.info(f'there\'s some issue while loading the params file [check main()]. exc: {e}')
      else : 
           # create dir if not present, else execute without any warning/error
           output_path = home_dir.as_posix() + params['load_dataset']['raw_data']
           pathlib.Path(output_path).mkdir(parents = True, exist_ok = True)
-          data = extract_data(params['load_dataset']['drive_link'])
-          save_data(data, output_path = output_path, file_name = params['load_dataset']['file_name'])
+          data = extract_data(bucket = params['load_dataset']['bucket'], key = params['load_dataset']['filename'],
+                              access_key = sc_params['aws_access_key_id'], secret_key = sc_params['aws_secret_access_key'])
+          save_data(data, output_path = output_path, file_name = params['load_dataset']['filename'])
           infologger.info('program terminated normally!')
 
 if __name__ == "__main__" : 
